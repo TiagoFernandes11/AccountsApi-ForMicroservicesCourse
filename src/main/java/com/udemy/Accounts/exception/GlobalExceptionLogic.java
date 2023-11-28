54b31ed0c375b761986976aa9b05439ed4993e41
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-11-28 10:54:44 -0300

    Update GlobalExceptionLogic.java

--- src/main/java/com/udemy/Accounts/exception/GlobalExceptionLogic.java
+++ src/main/java/com/udemy/Accounts/exception/GlobalExceptionLogic.java
@@ -1,8 +1,25 @@
 package com.udemy.Accounts.exception;
 
+import com.udemy.Accounts.dto.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionLogic extends RuntimeException {
-    //todo
+
+    public ResponseEntity<ErrorResponseDTO> handleCustomerAlreadyExistsExecption(CustomerAlreadyExistException exception,
+                                                                                 WebRequest webRequest){
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
+                webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
+    }
+
 }
