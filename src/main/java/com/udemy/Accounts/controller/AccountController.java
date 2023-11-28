59db7a7de27cff0dd8d12dcce47781e757a9519b
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-11-28 17:31:43 -0300

    First part of Swagger Documentation

--- src/main/java/com/udemy/Accounts/controller/AccountController.java
+++ src/main/java/com/udemy/Accounts/controller/AccountController.java
@@ -4,6 +4,10 @@ import com.udemy.Accounts.constants.AccountConstants;
 import com.udemy.Accounts.dto.CustomerDTO;
 import com.udemy.Accounts.dto.ResponseDTO;
 import com.udemy.Accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "CRUD REST APIs for Accounts in EazyBank",
+        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE account details"
+)
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @Validated
@@ -21,6 +29,14 @@ public class AccountController {
     @Autowired
     private IAccountsService iAccountsService;
 
+    @Operation(
+            summary = "CREATE account REST API",
+            description = "REST API to create new Customer & Account inside EazyBank"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status CREATED"
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createAccount(@Valid @RequestBody CustomerDTO customerDTO){
         iAccountsService.createAccount(customerDTO);
@@ -29,6 +45,14 @@ public class AccountController {
                 .body(new ResponseDTO(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "FETCH account REST API",
+            description = "REST API to fetch new Customer & Account details based on mobile number"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDTO> fetchAccountDetails(@RequestParam
                                                                @Pattern(regexp = "^$|[0-9]{10}", message = "AccountNumber must be 10 digits")
@@ -37,6 +61,21 @@ public class AccountController {
         return ResponseEntity.status(HttpStatus.OK). body(customerDTO);
     }
 
+    @Operation(
+            summary = "UPDATE account REST API",
+            description = "REST API to update Customer & Account inside EazyBank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @PutMapping("/update")
     public ResponseEntity<ResponseDTO> updateAccountDetails(@Valid @RequestBody CustomerDTO customerDTO){
         boolean isUptated = iAccountsService.updateAccount(customerDTO);
@@ -53,6 +92,21 @@ public class AccountController {
         }
     }
 
+    @Operation(
+            summary = "DELETE account REST API",
+            description = "REST API to delete a Customer & Account details based on mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDTO> deleteAccountDetails(@RequestParam
                                                                 @Pattern(regexp = "^$|[0-9]{10}", message = "AccountNumber must be 10 digits")
--- src/main/java/com/udemy/Accounts/dto/AccountsDTO.java
+++ src/main/java/com/udemy/Accounts/dto/AccountsDTO.java
@@ -1,18 +1,32 @@
 package com.udemy.Accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Account",
+        description = "Schema to hold Account information"
+)
 public class AccountsDTO {
+    @Schema(
+            description = "Number of Eazy Bank account"
+    )
     @NotEmpty
     @Pattern(regexp = "^$|[0-9]{10}", message = "AccountNumber must be 10 digits")
     private Integer accountNumber;
 
+    @Schema(
+            description = "Account type for Eazy Bank account", example = "Savings"
+    )
     @NotEmpty(message = "Account type can not be null or empty")
     private String accountType;
 
+    @Schema(
+            description = "Eazy Bank BranchAddress"
+    )
     @NotEmpty(message = "BranchAddress can not be null or empty")
     private String branchAddress;
 }
